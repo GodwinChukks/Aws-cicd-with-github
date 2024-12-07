86f874c2ec59b567bc4f7753c4d86b0150dacd9b
Godwin <godwinchukks@gmail.com>
2024-12-07 05:50:39 -0800

    updated

--- start_container.sh
+++ start_container.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 set -e
 
-# Debugging: Log environment details
-echo "Current PATH: $PATH"
-echo "Checking Docker location..."
-which docker || { echo "Docker is not in PATH"; exit 1; }
-docker --version || { echo "Docker is not accessible"; exit 1; }
+sudo su - root
 
-# Start Docker service if not already running
-if ! systemctl is-active --quiet docker; then
-    echo "Starting Docker service..."
-    sudo systemctl start docker
-fi
-
-# Pull the Docker image from Docker Hub
-echo "Pulling Docker image: godwinchukks/simple-python-flask-app"
+# pull the Docker image from Docker Hub
 docker pull godwinchukks/simple-python-flask-app
 
-# Run the Docker container
-echo "Running Docker container..."
+# run the docker image as container
 docker run -d -p 5000:5000 godwinchukks/simple-python-flask-app
